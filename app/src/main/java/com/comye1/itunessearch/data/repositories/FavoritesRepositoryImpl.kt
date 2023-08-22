bf71762dbecf2b76a5c056dc1a339b31e702588f
Yewon <devyewon@gmail.com>
2023-08-22 11:50:04 +0900

    #5 Repository 작성 (Favorites, Search)

---
+++ app/src/main/java/com/comye1/itunessearch/data/repositories/FavoritesRepositoryImpl.kt
@@ -0,0 +1,62 @@
+package com.comye1.itunessearch.data.repositories
+
+import com.comye1.itunessearch.data.local.FavoriteTrack
+import com.comye1.itunessearch.data.local.FavoritesDao
+import com.comye1.itunessearch.domain.FavoritesRepository
+import com.comye1.itunessearch.domain.Track
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class FavoritesRepositoryImpl @Inject constructor(
+    private val dao: FavoritesDao
+) : FavoritesRepository {
+    override fun getFavoriteTracks(): Flow<List<Track>> {
+        return dao.getAllTracks().map { list ->
+            list.map {
+                Track(
+                    id = it.id,
+                    artworkUrl = it.artworkUrl,
+                    trackName = it.trackName,
+                    collectionName = it.collectionName,
+                    artistName = it.artistName,
+                    favorite = true
+                )
+            }
+        }
+    }
+
+    override suspend fun addToFavorites(track: Track): Result<Boolean> {
+        try {
+            dao.insert(
+                FavoriteTrack(
+                    id = track.id,
+                    artworkUrl = track.artworkUrl,
+                    trackName = track.trackName,
+                    collectionName = track.collectionName,
+                    artistName = track.artistName,
+                )
+            )
+        } catch (e: Exception) {
+            return Result.failure(e)
+        }
+        return Result.success(true)
+    }
+
+    override suspend fun removeFromFavorites(track: Track): Result<Boolean> {
+        try {
+            dao.delete(
+                FavoriteTrack(
+                    id = track.id,
+                    artworkUrl = track.artworkUrl,
+                    trackName = track.trackName,
+                    collectionName = track.collectionName,
+                    artistName = track.artistName,
+                )
+            )
+        } catch (e: Exception) {
+            return Result.failure(e)
+        }
+        return Result.success(true)
+    }
+}
---
+++ app/src/main/java/com/comye1/itunessearch/domain/FavoritesRepository.kt
@@ -0,0 +1,9 @@
+package com.comye1.itunessearch.domain
+
+import kotlinx.coroutines.flow.Flow
+
+interface FavoritesRepository {
+    fun getFavoriteTracks(): Flow<List<Track>>
+    suspend fun addToFavorites(track: Track): Result<Boolean>
+    suspend fun removeFromFavorites(track: Track): Result<Boolean>
+}
